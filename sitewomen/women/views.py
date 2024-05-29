409b39617d4b6a07465a08ae4afb1862c6335a4f
Alexander Postnikov <alecpostnikov@mail.ru>
2024-05-29 16:36:24 +0300

    changes to views.py

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 
 def index(request):
@@ -15,6 +15,8 @@ def chocolate(request):
 
 
 def archive(request, year):
+    if int(year) > 2024:
+        raise Http404()
     return HttpResponse(f"{year} был тяжёлым")
 
 
